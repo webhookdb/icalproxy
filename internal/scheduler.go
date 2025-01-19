8fea4462042fd19963fdf63effc86bae6b5db260
Rob Galanakis <rob.galanakis@gmail.com>
2025-01-19 10:17:47 -0800

    Capture scheduler run errors in Sentry, add name

--- internal/scheduler.go
+++ internal/scheduler.go
@@ -2,7 +2,10 @@ package internal
 
 import (
 	"context"
+	"github.com/getsentry/sentry-go"
 	"github.com/lithictech/go-aperitif/v2/logctx"
+	"reflect"
+	"strings"
 	"time"
 )
 
@@ -11,20 +14,26 @@ type IRunner interface {
 }
 
 func StartScheduler(ctx context.Context, r IRunner, interval time.Duration) {
-	logctx.Logger(ctx).Info("starting_scheduler")
+	schedulerName := strings.ToLower(reflect.Indirect(reflect.ValueOf(r)).Type().Name())
+	ctx = logctx.AddTo(ctx, "scheduler", schedulerName)
+	logctx.Logger(ctx).Info("scheduler_starting")
 	go func() {
 		for {
-			logctx.Logger(ctx).Info("executing_scheduled")
+			logctx.Logger(ctx).Info("scheduler_starting_run")
 			if err := r.Run(ctx); err != nil {
-				logctx.Logger(ctx).With("error", err).Error("scheduled_job_error")
+				logctx.Logger(ctx).With("error", err).Error("scheduler_run_error")
+				sentry.WithScope(func(scope *sentry.Scope) {
+					scope.SetTag("scheduler", schedulerName)
+					sentry.CaptureException(err)
+				})
 			} else {
-				logctx.Logger(ctx).Info("executed_scheduled")
+				logctx.Logger(ctx).Info("scheduler_finished_run")
 			}
 			select {
 			case <-time.After(interval):
 				continue
 			case <-ctx.Done():
-				logctx.Logger(ctx).Info("canceling_scheduler")
+				logctx.Logger(ctx).Info("scheduler_closing")
 				return
 			}
 		}
