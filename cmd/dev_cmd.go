88a088c75e4e81dc64bf732a4685224ece6c31c3
Rob Galanakis <rob.galanakis@gmail.com>
2025-03-27 13:26:29 -0700

    Add `dev config` CLI command for debugging

--- cmd/dev_cmd.go
+++ cmd/dev_cmd.go
@@ -1,9 +1,12 @@
 package cmd
 
 import (
+	"fmt"
 	"github.com/aws/aws-sdk-go-v2/aws"
 	"github.com/aws/aws-sdk-go-v2/service/s3"
+	"github.com/lithictech/go-aperitif/v2/convext"
 	"github.com/urfave/cli/v2"
+	"github.com/webhookdb/icalproxy/config"
 	"github.com/webhookdb/icalproxy/feedstorage"
 )
 
@@ -22,6 +25,28 @@ var devCmd = &cli.Command{
 					Bucket: aws.String(appGlobals.Config.S3Bucket),
 				}); err != nil {
 					return err
+				}
+				return nil
+			},
+		},
+		{
+			Name:  "config",
+			Usage: "Print out config and exit. Used to help debug config loading behavior",
+			Action: func(c *cli.Context) error {
+				cfg, err := config.LoadConfig()
+				if err != nil {
+					return err
+				}
+				obj, err := convext.ToObject(cfg)
+				if err != nil {
+					fmt.Println("BuildTime:", config.BuildTime, "BuildSha:", config.BuildSha)
+					fmt.Printf("%+v\n", cfg)
+				} else {
+					fmt.Println("BuildTime:", config.BuildTime)
+					fmt.Println("BuildSha:", config.BuildSha)
+					for _, k := range convext.SortedObjectKeys(obj) {
+						fmt.Printf("%s: %v\n", k, obj[k])
+					}
 				}
 				return nil
 			},
